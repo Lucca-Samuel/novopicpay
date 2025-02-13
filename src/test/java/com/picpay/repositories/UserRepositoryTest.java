3dbb744b08b513856513f2827d0b0b1d46fba379
Lucca-Samuel <samuellucca1002@gmail.com>
2025-02-13 20:37:52 -0300

    feat: UserRepositoryTest

---
+++ src/test/java/com/picpay/repositories/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.picpay.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocument() {
+
+    }
+
+}
